5faea2ebf9ae642947544684cb7e7ea48b8bb7c0
ngsw877 <otsda1055@yahoo.co.jp>
2020-08-17 19:55:43 +0900

    投稿詳細画面の作成

--- laravel/routes/web.php
+++ laravel/routes/web.php
@@ -12,4 +12,5 @@
 */
 Auth::routes();
 Route::get('/', 'ArticleController@index')->name('articles.index');
-Route::resource('/articles', 'ArticleController')->except(['index'])->middleware('auth');
+Route::resource('/articles', 'ArticleController')->except(['index', 'show'])->middleware('auth');
+Route::resource('/articles', 'ArticleController')->only(['show']);
