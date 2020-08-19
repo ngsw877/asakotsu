33b2612c0af79725ed48682517e529149dcb0ad7
ngsw877 <otsda1055@yahoo.co.jp>
2020-08-20 07:33:15 +0900

    いいね時のハートアニメーションの追加

--- laravel/resources/js/components/ArticleLike.vue
+++ laravel/resources/js/components/ArticleLike.vue
@@ -5,7 +5,7 @@
       class="btn m-0 p-1 shadow-none"
     >
       <i class="fas fa-heart mr-1"
-      :class="{'red-text':this.isLikedBy}"
+      :class="{'red-text':this.isLikedBy, 'animated heartBeat fast':this.gotToLike}"
       @click="clickLike"
       />
     </button>
@@ -36,6 +36,7 @@
       return {
         isLikedBy: this.initialIsLikedBy,
         countLikes: this.initialCountLikes,
+        gotToLike: false
       }
     },
     methods: {
@@ -54,12 +55,14 @@
 
         this.isLikedBy = true
         this.countLikes = response.data.countLikes
+        this.gotToLike = true
       },
       async unlike() {
         const response = await axios.delete(this.endpoint)
 
         this.isLikedBy = false
         this.countLikes = response.data.countLikes
+        this.gotToLike = false
       },
     },
   }
