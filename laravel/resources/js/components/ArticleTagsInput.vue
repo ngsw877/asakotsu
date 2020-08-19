4ddccb407fa76dfdb9254b21f1ec40fc39e787b4
ngsw877 <otsda1055@yahoo.co.jp>
2020-08-20 08:09:13 +0900

    タグ入力のVueコンポーネントを追加(Vue Tags Inputを導入)

---
+++ laravel/resources/js/components/ArticleTagsInput.vue
@@ -0,0 +1,59 @@
+<template>
+  <div>
+    <vue-tags-input
+      v-model="tag"
+      :tags="tags"
+      :autocomplete-items="filteredItems"
+      @tags-changed="newTags => tags = newTags"
+    />
+  </div>
+</template>
+
+<script>
+import VueTagsInput from '@johmun/vue-tags-input';
+
+export default {
+  components: {
+    VueTagsInput,
+  },
+  data() {
+    return {
+      tag: '',
+      tags: [],
+      autocompleteItems: [{
+        text: 'Spain',
+      }, {
+        text: 'France',
+      }, {
+        text: 'USA',
+      }, {
+        text: 'Germany',
+      }, {
+        text: 'China',
+      }],
+    };
+  },
+  computed: {
+    filteredItems() {
+      return this.autocompleteItems.filter(i => {
+        return i.text.toLowerCase().indexOf(this.tag.toLowerCase()) !== -1;
+      });
+    },
+  },
+};
+</script>
+<style lang="css" scoped>
+  .vue-tags-input {
+    max-width: inherit;
+  }
+</style>
+<style lang="css">
+  .vue-tags-input .ti-tag {
+    background: transparent;
+    border: 1px solid #747373;
+    color: #747373;
+    margin-right: 4px;
+    border-radius: 0px;
+    font-size: 13px;
+  }
+</style>
--- laravel/resources/views/articles/form.blade.php
+++ laravel/resources/views/articles/form.blade.php
@@ -4,6 +4,11 @@
   <input type="text" name="title" class="form-control" required value="{{ $article->title ?? old('title') }}">
 </div>
 <div class="form-group">
+  <article-tags-input>
+    
+  </article-tags-input>
+</div>
+<div class="form-group">
   <label></label>
   <textarea name="body" required class="form-control" rows="16" placeholder="本文">{{ $article->body ?? old('body') }}</textarea>
 </div>
