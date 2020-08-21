97c0e2e20be4ff360a26ffff6b3cd004ae8d2d39
ngsw877 <otsda1055@yahoo.co.jp>
2020-08-22 07:06:59 +0900

    タグ入力フォームにて、スペースキーでもタグ確定ができるようカスタマイズ

--- laravel/resources/js/components/ArticleTagsInput.vue
+++ laravel/resources/js/components/ArticleTagsInput.vue
@@ -10,6 +10,7 @@
       :tags="tags"
       placeholder="タグを5個まで入力できます"
       :autocomplete-items="filteredItems"
+      :add-on-key="[13,32]"
       @tags-changed="newTags => tags = newTags"
     />
   </div>
