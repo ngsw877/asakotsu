dcdf6e81b128700284648bd6420f25767349a605
ngsw877 <otsda1055@yahoo.co.jp>
2020-08-23 16:47:07 +0900

    ZoomAPI連携

---
+++ laravel/app/Http/Controllers/Zoom/MeetingController.php
@@ -0,0 +1,136 @@
+<?php
+
+namespace App\Http\Controllers\Zoom;
+
+use App\Http\Controllers\Controller;
+use App\Traits\ZoomJWT;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+
+class MeetingController extends Controller
+{
+    use ZoomJWT;
+
+    const MEETING_TYPE_INSTANT = 1;
+    const MEETING_TYPE_SCHEDULE = 2;
+    const MEETING_TYPE_RECURRING = 3;
+    const MEETING_TYPE_FIXED_RECURRING_FIXED = 8;
+
+
+    public function list(Request $request)
+    {
+        $path = 'users/me/meetings';
+        $response = $this->zoomGet($path);
+
+        $data = json_decode($response->body(), true);
+        $data['meetings'] = array_map(function (&$m) {
+            $m['start_at'] = $this->toUnixTimeStamp($m['start_time'], $m['timezone']);
+            return $m;
+        }, $data['meetings']);
+
+        return [
+            'success' => $response->ok(),
+            'data' => $data,
+        ];
+    }
+
+    public function create(Request $request)
+{
+    $validator = Validator::make($request->all(), [
+        'topic' => 'required|string',
+        'start_time' => 'required|date',
+        'agenda' => 'string|nullable',
+    ]);
+
+    if ($validator->fails()) {
+        return [
+            'success' => false,
+            'data' => $validator->errors(),
+        ];
+    }
+    $data = $validator->validated();
+
+    $path = 'users/me/meetings';
+    $response = $this->zoomPost($path, [
+        'topic' => $data['topic'],
+        'type' => self::MEETING_TYPE_SCHEDULE,
+        'start_time' => $this->toZoomTimeFormat($data['start_time']),
+        'duration' => 30,
+        'agenda' => $data['agenda'],
+        'settings' => [
+            'host_video' => false,
+            'participant_video' => false,
+            'waiting_room' => true,
+        ]
+    ]);
+
+
+    return [
+        'success' => $response->status() === 201,
+        'data' => json_decode($response->body(), true),
+    ];
+}
+
+public function get(Request $request, string $id)
+{
+    $path = 'meetings/' . $id;
+    $response = $this->zoomGet($path);
+
+    $data = json_decode($response->body(), true);
+    if ($response->ok()) {
+        $data['start_at'] = $this->toUnixTimeStamp($data['start_time'], $data['timezone']);
+    }
+
+    return [
+        'success' => $response->ok(),
+        'data' => $data,
+    ];
+}
+
+public function update(Request $request, string $id)
+{
+    $validator = Validator::make($request->all(), [
+        'topic' => 'required|string',
+        'start_time' => 'required|date',
+        'agenda' => 'string|nullable',
+    ]);
+
+    if ($validator->fails()) {
+        return [
+            'success' => false,
+            'data' => $validator->errors(),
+        ];
+    }
+    $data = $validator->validated();
+
+    $path = 'meetings/' . $id;
+    $response = $this->zoomPatch($path, [
+        'topic' => $data['topic'],
+        'type' => self::MEETING_TYPE_SCHEDULE,
+        'start_time' => (new \DateTime($data['start_time']))->format('Y-m-d\TH:i:s'),
+        'duration' => 30,
+        'agenda' => $data['agenda'],
+        'settings' => [
+            'host_video' => false,
+            'participant_video' => false,
+            'waiting_room' => true,
+        ]
+    ]);
+
+    return [
+        'success' => $response->status() === 204,
+        'data' => json_decode($response->body(), true),
+    ];
+}
+
+public function delete(Request $request, string $id)
+{
+    $path = 'meetings/' . $id;
+    $response = $this->zoomDelete($path);
+
+    return [
+        'success' => $response->status() === 204,
+        'data' => json_decode($response->body(), true),
+    ];
+}
+}
---
+++ laravel/app/Traits/ZoomJWT.php
@@ -0,0 +1,87 @@
+<?php
+
+namespace App\Traits;
+
+use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\Http;
+use \GuzzleHttp\Client;
+
+trait ZoomJWT
+{
+    private function generateZoomToken()
+    {
+        $key = env('ZOOM_API_KEY', '');
+        $secret = env('ZOOM_API_SECRET', '');
+        $payload = [
+            'iss' => $key,
+            'exp' => strtotime('+1 minute'),
+        ];
+        return \Firebase\JWT\JWT::encode($payload, $secret, 'HS256');
+    }
+
+    private function retrieveZoomUrl()
+    {
+        return env('ZOOM_API_URL', '');
+    }
+
+    private function zoomRequest()
+    {
+        $jwt = $this->generateZoomToken();
+        $client = new Client(['base_uri' => 'https://api.zoom.us/v2/']);
+        $res = $client->request('GET', '/', [
+            'authorization' => 'Bearer ' . $jwt,
+            'content-type' => 'application/json',
+        ]);
+       return $res;
+    }
+
+    public function zoomGet(string $path, array $query = [])
+    {
+        $url = $this->retrieveZoomUrl();
+        $request = $this->zoomRequest();
+        return $request->get($url . $path, $query);
+    }
+
+    public function zoomPost(string $path, array $body = [])
+    {
+        $url = $this->retrieveZoomUrl();
+        $request = $this->zoomRequest();
+        return $request->post($url . $path, $body);
+    }
+
+    public function zoomPatch(string $path, array $body = [])
+    {
+        $url = $this->retrieveZoomUrl();
+        $request = $this->zoomRequest();
+        return $request->patch($url . $path, $body);
+    }
+
+    public function zoomDelete(string $path, array $body = [])
+    {
+        $url = $this->retrieveZoomUrl();
+        $request = $this->zoomRequest();
+        return $request->delete($url . $path, $body);
+    }
+
+    public function toZoomTimeFormat(string $dateTime)
+    {
+        try {
+            $date = new \DateTime($dateTime);
+            return $date->format('Y-m-d\TH:i:s');
+        } catch (\Exception $e) {
+            Log::error('ZoomJWT->toZoomTimeFormat : ' . $e->getMessage());
+            return '';
+        }
+    }
+
+    public function toUnixTimeStamp(string $dateTime, string $timezone)
+    {
+        try {
+            $date = new \DateTime($dateTime, new \DateTimeZone($timezone));
+            return $date->getTimestamp();
+        } catch (\Exception $e) {
+            Log::error('ZoomJWT->toUnixTimeStamp : ' . $e->getMessage());
+            return '';
+        }
+    }
+}
