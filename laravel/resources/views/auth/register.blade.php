c607e474b455a59aef70b655c46bd64be6b81ccf
ngsw877 <otsda1055@yahoo.co.jp>
2020-09-04 19:44:43 +0900

    ユーザーのプロフィール登録機能の追加

--- laravel/resources/views/auth/register.blade.php
+++ laravel/resources/views/auth/register.blade.php
@@ -17,12 +17,12 @@
             @include('error_card_list')
 
             <div class="card-text">
-              <form method="POST" action="{{ route('register') }}">
+              <form method="POST" action="{{ route('register') }} " enctype="multipart/form-data">
                 @csrf
                 <div class="md-form">
                   <label for="name">ユーザー名</label>
                   <input class="form-control" type="text" id="name" name="name" required value="{{ old('name') }}">
-                  <small>英数字3〜16文字(登録後の変更はできません)</small>
+                  <small>2〜16文字(登録後の変更はできません)</small>
                 </div>
                 <div class="md-form  mb-5">
                   <label for="email">メールアドレス</label>
@@ -36,6 +36,10 @@
                 <div class="md-form mb-5">
                   <label for="password_confirmation">パスワード(確認)</label>
                   <input class="form-control" type="password" id="password_confirmation" name="password_confirmation" required>
+                </div>
+                <div>
+                  <label for="profile_image">プロフィール画像</label>
+                  <input  type="file" id="profile_image" name="profile_image" accept="image/*">
                 </div>
                 <button class="btn btn-block peach-gradient mt-2 mb-2" type="submit">ユーザー登録</button>
               </form>
