3283b95b57e21edf0899e2cfbbd8c74643f43d36
ngsw877 <otsda1055@yahoo.co.jp>
2020-09-19 23:26:46 +0900

    ルーティングにコメント追加

--- laravel/routes/web.php
+++ laravel/routes/web.php
@@ -16,6 +16,7 @@ Auth::routes();
 // ゲストユーザーログイン
 Route::get('guest', 'Auth\LoginController@guestLogin')->name('login.guest');
 
+// ユーザー投稿関連機能(CRUD)
 Route::get('/', 'ArticleController@index')->name('articles.index');
 Route::resource('/articles', 'ArticleController')->except(['index', 'show'])->middleware('auth');
 Route::resource('/articles', 'ArticleController')->only(['show']);
@@ -23,10 +24,17 @@ Route::prefix('articles')->name('articles.')->group(function () {
   Route::put('/{article}/like', 'ArticleController@like')->name('like')->middleware('auth');
   Route::delete('/{article}/like', 'ArticleController@unlike')->name('unlike')->middleware('auth');
 });
+
+// 投稿のタグ機能
 Route::get('/tags/{name}', 'TagController@show')->name('tags.show');
+
+// ユーザー関連機能
 Route::prefix('users')->name('users.')->group(function () {
+  // ユーザー詳細表示
   Route::get('/{name}', 'UserController@show')->name('show');
+  // いいね機能
   Route::get('/{name}/likes', 'UserController@likes')->name('likes');
+  // フォロー・フォロワー機能
   Route::get('/{name}/followings', 'UserController@followings')->name('followings');
   Route::get('/{name}/followers', 'UserController@followers')->name('followers');
   Route::middleware('auth')->group(function () {
@@ -38,7 +46,7 @@ Route::prefix('users')->name('users.')->group(function () {
 // QiitaAPIテスト
 Route::get('/post', 'PostController@index');
 
-// ZoomAPI関連
+// Zoomミーティング関連機能(CRUD)
 Route::middleware('auth')->group(function () {
   Route::get('/meetings', 'Zoom\MeetingController@index')->name('meetings.index');
   Route::get('/meetings/create', 'Zoom\MeetingController@create')->name('meetings.create');
