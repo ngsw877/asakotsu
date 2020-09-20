a8c3689c68d1fba00f0803c3382253591f3c8c22
ngsw877 <otsda1055@yahoo.co.jp>
2020-09-20 17:21:58 +0900

    ミーティング新規作成ボタンの作成

--- laravel/resources/views/meetings/index.blade.php
+++ laravel/resources/views/meetings/index.blade.php
@@ -13,5 +13,11 @@
 
     @endforeach
   </div>
+  <div class="new-post">
+    <a class="new-post-btn" href="{{ route('meetings.create') }}">
+      <p>ミーティング作成</p>
+      <i class="fas fa-plus"></i>
+    </a>
+  </div>
 
 @endsection
