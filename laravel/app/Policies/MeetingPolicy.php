757e0e9bd03f74c81819dc2844c99a6ed2946333
ngsw877 <otsda1055@yahoo.co.jp>
2020-09-24 22:15:03 +0900

    [add]ミーティングの更新/削除への認可設定
    ポリシーの作成と、AuthServiceProviderの更新

---
+++ laravel/app/Policies/MeetingPolicy.php
@@ -0,0 +1,95 @@
+<?php
+
+namespace App\Policies;
+
+use App\Models\Meeting;
+use App\Models\User;
+use Illuminate\Auth\Access\HandlesAuthorization;
+
+class MeetingPolicy
+{
+    use HandlesAuthorization;
+
+    /**
+     * Determine whether the user can view any meetings.
+     *
+     * @param  \App\Models\User  $user
+     * @return mixed
+     */
+    public function viewAny(?User $user)
+    {
+        return true;
+    }
+
+    /**
+     * Determine whether the user can view the meeting.
+     *
+     * @param  \App\Models\User  $user
+     * @param  \App\Models\Meeting  $meeting
+     * @return mixed
+     */
+    public function view(?User $user, Meeting $meeting)
+    {
+        return true;
+    }
+
+    /**
+     * Determine whether the user can create meetings.
+     *
+     * @param  \App\Models\User  $user
+     * @return mixed
+     */
+    public function create(User $user)
+    {
+        return true;
+    }
+
+    /**
+     * Determine whether the user can update the meeting.
+     *
+     * @param  \App\Models\User  $user
+     * @param  \App\Models\Meeting  $meeting
+     * @return mixed
+     */
+    public function update(User $user, Meeting $meeting)
+    {
+        return $user->id === $meeting->user_id;
+    }
+
+    /**
+     * Determine whether the user can delete the meeting.
+     *
+     * @param  \App\Models\User  $user
+     * @param  \App\Models\Meeting  $meeting
+     * @return mixed
+     */
+    public function delete(User $user, Meeting $meeting)
+    {
+        return $user->id === $meeting->user_id;
+    }
+
+    /**
+     * Determine whether the user can restore the meeting.
+     *
+     * @param  \App\Models\User  $user
+     * @param  \App\Models\Meeting  $meeting
+     * @return mixed
+     */
+    public function restore(User $user, Meeting $meeting)
+    {
+        //
+    }
+
+    /**
+     * Determine whether the user can permanently delete the meeting.
+     *
+     * @param  \App\Models\User  $user
+     * @param  \App\Models\Meeting  $meeting
+     * @return mixed
+     */
+    public function forceDelete(User $user, Meeting $meeting)
+    {
+        //
+    }
+}
+
