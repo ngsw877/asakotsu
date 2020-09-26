c3b20fd31cb3b1e49e58c1ae302b9ff7ee709dd2
ngsw877 <otsda1055@yahoo.co.jp>
2020-09-26 19:46:54 +0900

    [update]フォロー中ユーザー一覧のSQL発行回数削減

--- laravel/app/Http/Controllers/UserController.php
+++ laravel/app/Http/Controllers/UserController.php
@@ -37,7 +37,7 @@ class UserController extends Controller
 
     public function followings(string $name)
     {
-        $user = User::where('name', $name)->first();
+        $user = User::where('name', $name)->first()->load('followings.followers');
         $followings = $user->followings->sortByDesc('created_at');
 
         return view('users.followings', [
