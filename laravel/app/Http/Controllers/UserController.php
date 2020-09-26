06efab82dbbee153f16e13ea15351279d6075dba
ngsw877 <otsda1055@yahoo.co.jp>
2020-09-26 19:35:17 +0900

    いいねした投稿一覧のSQL発行回数削減

--- laravel/app/Http/Controllers/UserController.php
+++ laravel/app/Http/Controllers/UserController.php
@@ -26,7 +26,7 @@ class UserController extends Controller
 
     public function likes(string $name)
     {
-        $user = User::where('name', $name)->first();
+        $user = User::where('name', $name)->first()->load(['likes.user', 'likes.likes', 'likes.tags']);
         $articles = $user->likes->sortByDesc('created_at');
 
         return view('users.likes', [
