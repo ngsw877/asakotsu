aae2c2bc5bcbd12574db829da7887b2e40847988
ngsw877 <otsda1055@yahoo.co.jp>
2020-09-26 19:49:38 +0900

    [update]フォロワー覧のSQL発行回数削減

--- laravel/app/Http/Controllers/UserController.php
+++ laravel/app/Http/Controllers/UserController.php
@@ -48,7 +48,7 @@ class UserController extends Controller
 
     public function followers(string $name)
     {
-        $user = User::where('name', $name)->first();
+        $user = User::where('name', $name)->first()->load('followers.followers');;
         $followers = $user->followers->sortByDesc('created_at');
 
         return view('users.followers', [
