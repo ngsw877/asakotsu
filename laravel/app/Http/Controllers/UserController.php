0e3c2db9cab9cb76707c1d0d18ba2eef996eff0e
ngsw877 <otsda1055@yahoo.co.jp>
2020-09-26 15:19:30 +0900

    [update]ユーザーページ投稿一覧のSQL発行回数削減

--- laravel/app/Http/Controllers/UserController.php
+++ laravel/app/Http/Controllers/UserController.php
@@ -15,7 +15,7 @@ class UserController extends Controller
 {
     public function show(string $name)
     {
-        $user = User::where('name', $name)->first();
+        $user = User::where('name', $name)->first()->load(['articles.user', 'articles.likes', 'articles.tags']);
         $articles = $user->articles->sortByDesc('created_at');
 
         return view('users.show', [
