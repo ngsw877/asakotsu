a2564f49738c5030e1d5cf5c52e3dc5b3b8d736c
ngsw877 <otsda1055@yahoo.co.jp>
2020-10-03 17:29:11 +0900

    [add]投稿画面表示機能のテストコードを追加

--- laravel/tests/Feature/ArticleControllerTest.php
+++ laravel/tests/Feature/ArticleControllerTest.php
@@ -2,6 +2,7 @@
 
 namespace Tests\Feature;
 
+use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
@@ -16,5 +17,23 @@ class ArticleControllerTest extends TestCase
 
         $response->assertStatus(200)
         ->assertViewIs('articles.index');
+    }
+
+    public function testGuestCreate()
+    {
+        $response = $this->get(route('articles.create'));
+
+        $response->assertRedirect('login');
+    }
+
+    public function testAuthCreate()
+    {
+        $user = factory(User::class)->create();
+
+        $response = $this->actingAs($user)
+        ->get(route('articles.create'));
+
+        $response->assertStatus(200)
+        ->assertViewIs('articles.create');
     }
 }
