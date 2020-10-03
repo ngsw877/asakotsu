265192348b8056ca00bf385c3c0ded1448eeed6d
ngsw877 <otsda1055@yahoo.co.jp>
2020-10-03 22:44:23 +0900

    [update]usersテーブルのseederを修正

--- laravel/database/seeds/UsersTableSeeder.php
+++ laravel/database/seeds/UsersTableSeeder.php
@@ -3,6 +3,7 @@
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
 
 class UsersTableSeeder extends Seeder
 {
@@ -19,19 +20,43 @@ class UsersTableSeeder extends Seeder
             'email' => 'guest@guest.com',
             'password' => Hash::make(config('user.user_password_01')),
             'profile_image' => 'default.png',
+            'remember_token' => Str::random(10),
+            'created_at' => now(),
+            'updated_at' => now(),
             ],
             [
             'name' => '山田',
             'email' => 'yamada@co.jp',
             'password' => Hash::make(config('user.user_password_02')),
             'profile_image' => 'sun.gif',
+            'remember_token' => Str::random(10),
+            'created_at' => now(),
+            'updated_at' => now(),
             ],
             [
             'name' => 'suzuki',
             'email' => 'suzuki@co.jp',
             'password' => Hash::make(config('user.user_password_03')),
             'profile_image' => 'cat.jpg',
+            'remember_token' => Str::random(10),
+            'created_at' => now(),
+            'updated_at' => now(),
             ],
         ]);
+
+        for($i=4; $i <= 10; $i++) {
+            DB::table('users')->insert([
+                [
+                'name' => 'test_user' . $i,
+                'email' => 'test' .$i .'@test.com',
+                'password' => Hash::make(config('12345678')),
+                'profile_image' => 'https://placehold.jp/50x50.png',
+                'remember_token' => Str::random(10),
+                'created_at' => now(),
+                'updated_at' => now(),
+                ],
+            ]);
+        }
+
     }
 }
