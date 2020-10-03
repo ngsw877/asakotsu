7b3138cb48340ecdba49933ec8aa17a2fb967d98
ngsw877 <otsda1055@yahoo.co.jp>
2020-10-03 20:09:51 +0900

    [mod]いいねされているかを判定するメソッドのテストコードを追加

--- laravel/tests/Feature/ArticleTest.php
+++ laravel/tests/Feature/ArticleTest.php
@@ -3,6 +3,7 @@
 namespace Tests\Feature;
 
 use App\Models\Article;
+use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
@@ -16,6 +17,29 @@ class ArticleTest extends TestCase
         $article = factory(Article::class)->create();
 
         $result = $article->isLikedBy(null);
+
+        $this->assertFalse($result);
+    }
+
+    public function testIsLikedByTheUser()
+    {
+        $article = factory(Article::class)->create();
+        $user = factory(User::class)->create();
+        $article->likes()->attach($user);
+
+        $result = $article->isLikedBy($user);
+
+        $this->assertTrue($result);
+    }
+
+    public function testIsLikedByAnother()
+    {
+        $article = factory(Article::class)->create();
+        $user = factory(User::class)->create();
+        $another = factory(User::class)->create();
+        $article->likes()->attach($another);
+
+        $result = $article->isLikedBy($user);
 
         $this->assertFalse($result);
     }
