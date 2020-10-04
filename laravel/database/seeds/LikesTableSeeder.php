d1f5c3a9cc8aa7df3e47059fc35dc5f254f70ff2
ngsw877 <otsda1055@yahoo.co.jp>
2020-10-04 15:52:20 +0900

    [update]seeder修正

--- laravel/database/seeds/LikesTableSeeder.php
+++ laravel/database/seeds/LikesTableSeeder.php
@@ -5,12 +5,20 @@ use Illuminate\Support\Facades\DB;
 
 class LikesTableSeeder extends Seeder
 {
-    // ユーザID 1 が、自分を除くツイートに対して1ついいねを付ける
     public function run()
     {
+        // ユーザID 1 が、自分を除くツイートに対して1ついいねを付ける
         for($i = 2; $i <= 10; $i++) {
             DB::table('likes')->insert([
                 'user_id' => 1,
+                'article_id' => $i
+                ]);
+            }
+            
+        // ユーザID 2 が、ユーザー 1 と自分を除くツイートに対して1ついいねを付ける
+        for($i = 3; $i <= 10; $i++) {
+            DB::table('likes')->insert([
+                'user_id' => 2,
                 'article_id' => $i
                 ]);
             }
