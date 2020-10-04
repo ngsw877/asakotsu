390f20c2a2790464ff5b4a058e49cd09a10fee26
ngsw877 <otsda1055@yahoo.co.jp>
2020-10-04 21:13:05 +0900

    [add]投稿へのコメント表示機能の追加

--- laravel/resources/views/articles/show.blade.php
+++ laravel/resources/views/articles/show.blade.php
@@ -8,6 +8,33 @@
 
   <div class="container">
     @include('articles.card')
+
+    <div class="row justify-content-center">
+        <div class="col-md-8 mb-3">
+            <ul class="list-group">
+                @forelse ($comments as $comment)
+                    <li class="list-group-item">
+                        <div class="py-3 w-100 d-flex">
+                            <img class="profile-icon rounded-circle" src="{{ asset('/images/profile/' .$comment->user->profile_image) }}" alt="プロフィールアイコン">
+                            <div class="ml-2 d-flex flex-column">
+                                <p class="mb-0">{{ $comment->user->name }}</p>
+                            </div>
+                            <div class="d-flex justify-content-end flex-grow-1">
+                                <p class="mb-0 text-secondary">{{ $comment->created_at->format('Y-m-d H:i') }}</p>
+                            </div>
+                        </div>
+                        <div class="py-3">
+                            {!! nl2br(e($comment->comment)) !!}
+                        </div>
+                    </li>
+                @empty
+                    <li class="list-group-item mt-3">
+                        <p class="mb-0 text-secondary">コメントはまだありません。</p>
+                    </li>
+                @endforelse
+            </ul>
+        </div>
+    </div>
   </div>
 
 @endsection
