910365e5df38e8f77104e65da940a87daecbf407
ngsw877 <otsda1055@yahoo.co.jp>
2020-10-06 15:19:48 +0900

    [clean]ルーティングの整理

--- laravel/routes/web.php
+++ laravel/routes/web.php
@@ -11,25 +11,20 @@
 |
 */
 
-// ユーザー新規登録、ログイン、ログアウト
+# ユーザー新規登録、ログイン、ログアウト
 Auth::routes();
-// ゲストユーザーログイン
+
+# ゲストユーザーログイン
 Route::get('guest', 'Auth\LoginController@guestLogin')->name('login.guest');
 
-// ユーザー投稿関連機能(CRUD)
+# ユーザー投稿関係(index, show)
 Route::get('/', 'ArticleController@index')->name('articles.index');
-Route::resource('/articles', 'ArticleController')->except(['index', 'show'])->middleware('auth');
 Route::resource('/articles', 'ArticleController')->only(['show']);
-// いいね機能
-Route::prefix('articles')->name('articles.')->group(function () {
-  Route::put('/{article}/like', 'ArticleController@like')->name('like')->middleware('auth');
-  Route::delete('/{article}/like', 'ArticleController@unlike')->name('unlike')->middleware('auth');
-});
 
-// 投稿のタグ機能
+# 投稿のタグ機能
 Route::get('/tags/{name}', 'TagController@show')->name('tags.show');
 
-// ユーザー関連機能
+# ユーザー関連機能
 Route::prefix('users')->name('users.')->group(function () {
   // ユーザー詳細表示
   Route::get('/{name}', 'UserController@show')->name('show');
@@ -39,24 +34,37 @@ Route::prefix('users')->name('users.')->group(function () {
   Route::get('/{name}/followings', 'UserController@followings')->name('followings');
   // フォロワー一覧を表示
   Route::get('/{name}/followers', 'UserController@followers')->name('followers');
-  // フォロー機能
-  Route::middleware('auth')->group(function () {
-    Route::put('/{name}/follow', 'UserController@follow')->name('follow');
-    Route::delete('/{name}/follow', 'UserController@unfollow')->name('unfollow');
-  });
+
 });
 
-// コメント機能
-Route::resource('/comments', 'CommentController')->only(['store'])->middleware('auth');
-
-// Zoomミーティング関連機能(CRUD)
-Route::middleware('auth')->group(function () {
-  Route::resource('/meetings', 'Zoom\MeetingController');
-});
-
-// テスト用ルーティング
+# テスト用ルーティング
 Route::get('/test', function() {
   return view('test');
 });
 
 
+# ログイン状態で使用可能
+Route::group(['middleware' => 'auth'], function() {
+
+  // ユーザー投稿関係(create, edit, update, destroy)
+  Route::resource('/articles', 'ArticleController')->except(['index', 'show']);
+
+  // いいね機能
+  Route::prefix('articles')->name('articles.')->group(function () {
+    Route::put('/{article}/like', 'ArticleController@like')->name('like');
+    Route::delete('/{article}/like', 'ArticleController@unlike')->name('unlike');
+  });
+
+  // フォロー機能
+  Route::prefix('users')->name('users.')->group(function () {
+    Route::put('/{name}/follow', 'UserController@follow')->name('follow');
+    Route::delete('/{name}/follow', 'UserController@unfollow')->name('unfollow');
+  });
+
+  // コメント機能
+  Route::resource('/comments', 'CommentController')->only(['store']);
+
+  // Zoomミーティング関連機能(CRUD)
+  Route::resource('/meetings', 'Zoom\MeetingController');
+
+});
