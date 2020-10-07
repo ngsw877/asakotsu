62132002bfda03e7fab7ae89e3e3ced1abfa9a35
ngsw877 <otsda1055@yahoo.co.jp>
2020-10-07 20:48:03 +0900

    [update]投稿ボタンの色を変更

--- laravel/resources/views/articles/index.blade.php
+++ laravel/resources/views/articles/index.blade.php
@@ -14,7 +14,7 @@
     @endforeach
   </div>
   <div class="new-post">
-    <a class="new-post-btn" href="{{ route('articles.create') }}">
+    <a class="new-article-btn" href="{{ route('articles.create') }}">
       <p>新規投稿</p>
       <i class="fas fa-plus"></i>
     </a>
