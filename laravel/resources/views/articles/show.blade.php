a24a4864575a57f4ae5d39cdb0583648e974b155
ngsw877 <otsda1055@yahoo.co.jp>
2020-10-22 23:46:23 +0900

    [update]コメント投稿ボタンの色をベースカラー寄りに修正

--- laravel/resources/views/articles/show.blade.php
+++ laravel/resources/views/articles/show.blade.php
@@ -82,7 +82,7 @@
                                 <div class="form-group row mb-0">
                                     <div class="col-md-12 text-right">
                                         <p class="mb-4 text-danger">140文字以内</p>
-                                        <button type="submit" class="btn btn-primary">
+                                        <button type="submit" class="btn peach-gradient">
                                             コメントする
                                         </button>
                                     </div>
