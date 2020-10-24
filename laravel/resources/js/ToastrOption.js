7aa8d766509425aafc5d285b34a58ff89e27cd2b
ngsw877 <otsda1055@yahoo.co.jp>
2020-10-24 15:49:10 +0900

    [update]フラッシュメッセージのデザイン修正(toastrオプション変更)

---
+++ laravel/resources/js/ToastrOption.js
@@ -0,0 +1,22 @@
+$(function() {
+
+  //ドキュメントロード時に、toastr のオプションを設定する
+  toastr.options = {
+  "closeButton": true,
+  "debug": false,
+  "newestOnTop": false,
+  "progressBar": true,
+  "positionClass": "toast-top-full-width",
+  "preventDuplicates": false,
+  "onclick": null,
+  "showDuration": "300",
+  "hideDuration": "1000",
+  "timeOut": "2000",
+  "extendedTimeOut": "1000",
+  "showEasing": "swing",
+  "hideEasing": "linear",
+  "showMethod": "fadeIn",
+  "hideMethod": "fadeOut"
+  }
+
+});
