c569940a7d9351498d0cf960e6645c97f110fae1
ngsw877 <otsda1055@yahoo.co.jp>
2020-10-25 22:29:02 +0900

    [add]サイドバーに朝活Zoomのリンクボタンを設置

---
+++ laravel/resources/views/components/zoom_link.blade.php
@@ -0,0 +1,10 @@
+<div class="card mb-4">
+    <div class="btn blue-gradient text-center m-0">
+        <a href="{{ route('meetings.index') }}" class="text-white">
+          <p class="h4">Zoom朝活</p>
+          <p class="h1">
+            <i class="fas fa-video"></i>
+          </p>
+        </a>
+    </div>
+</div>
