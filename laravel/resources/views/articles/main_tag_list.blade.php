50bf17522e26e36c7763b7a0ffd67c6111d7bc0c
ngsw877 <otsda1055@yahoo.co.jp>
2020-10-27 14:52:39 +0900

    [update]トップページデザインの修正

--- laravel/resources/views/articles/main_tag_list.blade.php
+++ laravel/resources/views/articles/main_tag_list.blade.php
@@ -1,6 +1,6 @@
 <div class="card mb-4 sidebar-content">
     <div class="card-header text-center"># メインタグ</div>
-    <div class="card-body main-tag-list">
+    <div class="card-body main-tag-list py-3">
         <a href="">
             <p>#行動宣言</p>
         </a>
--- laravel/resources/views/articles/user_ranking_list.blade.php
+++ laravel/resources/views/articles/user_ranking_list.blade.php
@@ -1,9 +1,14 @@
 <div class="card mb-4 sidebar-content">
-    <div class="card-header text-center">
-        <i class="fas fa-crown mr-2 text-warning"></i>
-        <span class="font-weight-bold">{{ date('m') }}月</span>の早起き達成日数ランキング
+    <div class="card-header d-flex align-items-center">
+        <p class="m-0 text-center mx-auto">
+            <i class="fas fa-crown mr-2 text-warning fa-lg"></i>
+            <span class="mr-1">早起き達成日数ランキング</span>
+            <span class="p-1 rounded font-weight-bold d-inline-block sunny-morning-gradient text-white">
+                {{ date('m') }}月
+            </span>
+        </p>
     </div>
-    <div class="card-body user-ranking-list">
+    <div class="card-body user-ranking-list py-3">
         @foreach ($ranked_users  as $key => $ranked_user)
             <div class="d-flex justify-content-between">
                 <p class="ranking-icon{{ $key + 1 }}">
@@ -12,7 +17,7 @@
                 <a  class="block" href="{{ route('users.show', ['name' => $ranked_user->name]) }}">
                     <p>{{$ranked_user->name}}さん</p>
                 </a>
-                <p class="h5">{{ $ranked_user->achievement_days_count }}日</p>
+                <p class="h5">{{ $ranked_user->achievement_days_count }} 日</p>
             </div>
         @endforeach
     </div>
