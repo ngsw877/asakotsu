bb58fe44a2db0d681f7df30a29e02cc905a75ecb
ngsw877 <otsda1055@yahoo.co.jp>
2020-10-29 16:12:30 +0900

    [update]Zoomミーティング一覧のデザインをブラッシュアップ

--- laravel/resources/views/meetings/card.blade.php
+++ laravel/resources/views/meetings/card.blade.php
@@ -1,10 +1,11 @@
-<div class="card mt-4">
-  <div class="card-header d-flex flex-row align-items-center">
+<div class="card mb-4">
+  <div class="card-header d-flex flex-row align-items-center blue-gradient text-white">
     <a href="{{ route('users.show', ['name' => $meeting->user->name]) }}" class="text-dark m">
       <img class="user-icon rounded-circle mr-3" src="/images/profile/{{ $meeting->user->profile_image }}">
     </a>
-    <a href="{{ route('users.show', ['name' => $meeting->user->name]) }}" class="text-dark">
+    <a href="{{ route('users.show', ['name' => $meeting->user->name]) }}" class="text-white">
       <strong>{{ $meeting->user->name }}</strong> &nbsp;さんのミーティング
+      <i class="fas fa-video ml-2"></i>
     </a>
 
      @if( Auth::id() === $meeting->user_id )
@@ -12,7 +13,7 @@
       <div class="ml-auto card-text">
         <div class="dropdown">
           <a data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-            <i class="fas fa-ellipsis-v"></i>
+            <i class="fas fa-ellipsis-v fa-lg"></i>
           </a>
           <div class="dropdown-menu dropdown-menu-right">
             <a class="dropdown-item" href="{{ route('meetings.edit', ['meeting' => $meeting]) }}">
@@ -39,8 +40,8 @@
             <form method="POST" action="{{ route('meetings.destroy', ['meeting' => $meeting]) }}">
               @csrf
               @method('DELETE')
-              <div class="modal-body">
-                {{ $meeting->topic }}を削除します。よろしいですか？
+              <div class="modal-body text-center text-dark">
+                {{ $meeting->topic }} を削除します。よろしいですか？
               </div>
               <div class="modal-footer justify-content-between">
                 <a class="btn btn-outline-grey" data-dismiss="modal">キャンセル</a>
@@ -53,27 +54,32 @@
       <!-- modal -->
     @endif
   </div>
-  <div class="card-body pt-0">
-    <table class="table">
+  <div class="card-body">
+    <table class="table table w-75 mx-auto">
       <tbody class="container">
         <tr class="row">
-          <th scope="row" class="col-3">ミーティング名</th>
-          <td class="col-9"> {{ $meeting->topic }}</th>
+          <th scope="row" class="col-4 border-top-0 font-weight-bold">ミーティング名</th>
+          <td class="col-8 border-top-0"> {{ $meeting->topic }}</th>
         </tr>
+        @isset($meeting->agenda)
         <tr class="row">
-          <th scope="row" class="col-3" scope="">テーマ</th>
-          <td class="col-9">{{ $meeting->agenda }}</td>
+          <th scope="row" class="col-4 font-weight-bold">テーマ</th>
+          <td class="col-8">{{ $meeting->agenda }}</td>
         </tr>
+        @endisset
         <tr class="row">
-          <th scope="row" class="col-3" scope="">開始日時</th>
-          <td class="col-9">{{ date('Y/m/d　H時i分', strtotime($meeting->start_time)) }}&nbsp;〜</td>
+          <th scope="row" class="col-4 font-weight-bold">開始日時</th>
+          <td class="col-8">
+            <i class="fas fa-clock mr-2 text-primary"></i>
+            {{ date('Y/m/d　H時i分', strtotime($meeting->start_time)) }}&nbsp;〜
+          </td>
         </tr>
         @if( Auth::id() === $meeting->user_id )
           <tr class="row">
-            <th scope="row" class="col-3" scope="">
+            <th scope="row" class="col-4 font-weight-bold">
               開始URL
             </th>
-            <td class="col-9 ">
+            <td class="col-8 ">
                 <a class="text-primary" href="{{ $meeting->start_url }}">
                   {{ substr($meeting->start_url, 0, 75) }} ...
                 </a>
@@ -83,8 +89,8 @@
           </tr>
         @endif
         <tr class="row">
-          <th scope="row" class="col-3" scope="">参加URL</th>
-          <td class="col-9">
+          <th scope="row" class="col-4 font-weight-bold">参加URL</th>
+          <td class="col-8">
             <a class="text-primary" href="{{ $meeting->join_url }}">
               {{ $meeting->join_url }}
             </a>
--- laravel/resources/views/meetings/index.blade.php
+++ laravel/resources/views/meetings/index.blade.php
@@ -6,20 +6,24 @@
 
   @include('nav')
 
-  <div class="container mt-2 mb-5">
+  <div class="container mt-4">
 
-    @include('meetings.list', compact('meetings'))
+    <div class="row">
+     <div class="mx-auto col-md-8">
+        @include('meetings.list', compact('meetings'))
 
-    <!-- 無限スクロールのsppiner -->
-    @if ($meetings->nextPageUrl())
-      <a href="{{ $meetings->nextPageUrl() }}" infinity-scroll>
-        <div class="d-flex justify-content-center my-4">
-          <div class="spinner-border" role="status">
-            <span class="sr-only">Loading...</span>
-          </div>
-        </div>
-      </a>
-    @endif
+        <!-- 無限スクロールのsppiner -->
+        @if ($meetings->nextPageUrl())
+          <a href="{{ $meetings->nextPageUrl() }}" infinity-scroll>
+            <div class="d-flex justify-content-center my-4">
+              <div class="spinner-border" role="status">
+                <span class="sr-only">Loading...</span>
+              </div>
+            </div>
+          </a>
+        @endif
+     </div>
+    </div>
 
   </div>
   <div class="new-post">
