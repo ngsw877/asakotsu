549c5c39005136d8314d2b8d7d12a4f4ee59f0ee
ngsw877 <otsda1055@yahoo.co.jp>
2020-10-29 15:19:51 +0900

    [update]ユーザー投稿画面、投稿編集画面のデザインをブラッシュアップ

--- laravel/resources/views/articles/create.blade.php
+++ laravel/resources/views/articles/create.blade.php
@@ -5,16 +5,17 @@
 @include('nav')
 
 @section('content')
-  <div class="container">
+  <div class="container my-5">
     <div class="row">
-      <div class="col-12">
-        <div class="card mt-3">
-          <div class="card-body pt-0">
+      <div class="mx-auto col-md-8">
+        <div class="card">
+          <h2 class="h4 card-header text-center sunny-morning-gradient text-white">投稿する</h2>
+          <div class="card-body pt-3">
 
             @include('error_card_list')
 
             <div class="card-text">
-              <form method="POST" action="{{ route('articles.store') }}">
+              <form method="POST" class="w-75 mx-auto" action="{{ route('articles.store') }}">
 
                 @include('articles.form')
 
