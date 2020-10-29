3e4f8e8e8e4d1080199ba3b340c5cf7174bf970c
ngsw877 <otsda1055@yahoo.co.jp>
2020-10-29 23:45:08 +0900

    [update]ユーザーの早起き達成日数のランキング順位の数値を正確に表示するよう修正
    複数ユーザーの早起き達成日数が等しい(タイ)場合、ランキング順位も同じにするよう処理を修正

--- laravel/resources/views/articles/user_ranking_list.blade.php
+++ laravel/resources/views/articles/user_ranking_list.blade.php
@@ -1,24 +1,34 @@
 <div class="card mb-4 sidebar-content">
     <div class="card-header d-flex align-items-center row m-0 text-center">
-            <div class="col-10 pl-0">
-                <i class="fas fa-crown mr-2 text-warning fa-lg"></i>
-                <span class="mr-1">早起き達成日数ランキング</span>
-            </div>
-            <div class="col-2 p-1 rounded sunny-morning-gradient d-flex align-items-center justify-content-center font-weight-bold text-white">
-                <span class="font-weight-bold text-white">{{ date('m') }}月</span>
-            </div>
+        <div class="col-10 pl-0">
+            <i class="fas fa-crown mr-2 text-warning fa-lg"></i>
+            <span class="mr-1">早起き達成日数ランキング</span>
+        </div>
+        <div class="col-2 p-1 rounded sunny-morning-gradient d-flex align-items-center justify-content-center font-weight-bold text-white">
+            <span class="font-weight-bold text-white">{{ date('m') }}月</span>
+        </div>
     </div>
     <div class="card-body user-ranking-list py-3">
-        @foreach ($ranked_users  as $key => $ranked_user)
-            <div class="d-flex justify-content-between">
-                <p class="ranking-icon{{ $key + 1 }}">
-                    {{ $key + 1 }}
-                </p>
-                <a  class="block" href="{{ route('users.show', ['name' => $ranked_user->name]) }}">
-                    <p>{{$ranked_user->name}}さん</p>
-                </a>
-                <p class="h5">{{ $ranked_user->achievement_days_count }} 日</p>
-            </div>
+        @php
+        $rank = 1;
+        $before = $ranked_users->first()->achievement_days_count;
+        @endphp
+        @foreach ($ranked_users as $ranked_user)
+        @php
+        if ($before > $ranked_user->achievement_days_count) {
+        $rank++;
+        $before = $ranked_user->achievement_days_count;
+        }
+        @endphp
+        <div class="d-flex justify-content-between">
+            <p class="ranking-icon{{ $rank }}">
+                {{ $rank}}
+            </p>
+            <a class="block" href="{{ route('users.show', ['name' => $ranked_user->name]) }}">
+                <p>{{$ranked_user->name}}さん</p>
+            </a>
+            <p class="h5">{{ $ranked_user->achievement_days_count }} 日</p>
+        </div>
         @endforeach
     </div>
 </div>
