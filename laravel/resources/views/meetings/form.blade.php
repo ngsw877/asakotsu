6b5fb740317c19e2d36e63713dc98c899d62866a
ngsw877 <otsda1055@yahoo.co.jp>
2020-11-01 11:27:32 +0900

    [update]Zoomミーティング作成画面のデザインをブラッシュアップ

--- laravel/resources/views/meetings/form.blade.php
+++ laravel/resources/views/meetings/form.blade.php
@@ -1,15 +1,22 @@
 @csrf
-<div class="form-group h5">
-  <label for="topic">ミーティング名</label>
+<div class="form-group">
+  <label for="topic">
+    ミーティング名
+    <small class="text-danger">（必須）</small>
+  </label>
   <input class="form-control" type="text" id="topic" name="topic" value="{{ $meeting->topic ?? old('topic') }}">
 </div>
-<div class="form-group h5">
-  <label for="agenda">テーマ</label>
+<div class="form-group">
+  <label for="agenda">
+    テーマ
+    <small class="blue-grey-text">（任意）</small>
+  </label>
   <input class="form-control" type="text" id="agenda" name="agenda" value="{{ $meeting->agenda ?? old('agenda') }}">
 </div>
-<div class="row">
-  <div class="form-group h5 col-xs-2 mx-auto">
-    <label for="start_time">開始日時</label>
-    <input class="form-control" type="datetime-local" id="start_time" name="start_time" value="{{ $meeting->start_time ?? old('start_time') }}">
-  </div>
+<div class="form-group w-50">
+  <label for="start_time">
+    開始日時
+    <small class="text-danger">（必須）</small>
+  </label>
+  <input class="form-control" type="datetime-local" id="start_time" name="start_time" value="{{ $meeting->start_time ?? old('start_time') }}">
 </div>
