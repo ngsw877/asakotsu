39a4327b1a08da871d0b5ec1d8e7976b0f77852a
ngsw877 <otsda1055@yahoo.co.jp>
2020-11-03 22:41:29 +0900

    [update]CloudFrontのHTTPS化に対応するよう、プロキシ設定を変更

--- laravel/app/Http/Middleware/TrustProxies.php
+++ laravel/app/Http/Middleware/TrustProxies.php
@@ -12,12 +12,18 @@ class TrustProxies extends Middleware
      *
      * @var array|string
      */
-    protected $proxies;
+    protected $proxies = '**';
 
     /**
      * The headers that should be used to detect proxies.
      *
      * @var int
      */
-    protected $headers = Request::HEADER_X_FORWARDED_ALL;
+    protected $headers = [
+        Request::HEADER_FORWARDED => 'FORWARDED',
+        Request::HEADER_X_FORWARDED_FOR => 'X_FORWARDED_FOR',
+        Request::HEADER_X_FORWARDED_HOST => 'X_FORWARDED_HOST',
+        Request::HEADER_X_FORWARDED_PORT => 'X_FORWARDED_PORT',
+        Request::HEADER_X_FORWARDED_PROTO => 'X_FORWARDED_PROTO',
+    ];
 }
