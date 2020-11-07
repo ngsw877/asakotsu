f2101b6d41e453e8d7181c69ed9355cc789cee32
ngsw877 <otsda1055@yahoo.co.jp>
2020-11-07 14:09:50 +0900

    [clean]ユーザー投稿一覧の無限スクロールスピナーをBlade化

--- laravel/resources/views/users/likes.blade.php
+++ laravel/resources/views/users/likes.blade.php
@@ -10,19 +10,12 @@
     <div class="row justify-content-center">
       <div class="col-md-9">
         @include('users.user')
+        
         @include('users.tabs', ['hasArticles' => false, 'hasLikes' => true])
 
         @include('articles.list', compact('articles'))
-        <!-- 無限スクロールのsppiner -->
-        @if ($articles->nextPageUrl())
-          <a href="{{ $articles->nextPageUrl() }}" infinity-scroll>
-            <div class="d-flex justify-content-center my-4">
-              <div class="spinner-border" role="status">
-                <span class="sr-only">Loading...</span>
-              </div>
-            </div>
-          </a>
-        @endif
+
+        @include('articles.sppiner')
       </div>
     </div>
   </div>
