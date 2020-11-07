19cf3a4816dc657cbeffc7f943788c21bc6e7da5
ngsw877 <otsda1055@yahoo.co.jp>
2020-11-07 11:11:35 +0900

    [upate]ユーザープロフィールの、いいねした投稿一覧の幅を調整

--- laravel/resources/views/users/likes.blade.php
+++ laravel/resources/views/users/likes.blade.php
@@ -3,13 +3,19 @@
 @section('title', $user->name . 'さんのいいねした投稿')
 
 @section('content')
+
   @include('nav')
-  <div class="container">
-    @include('users.user')
-    @include('users.tabs', ['hasArticles' => false, 'hasLikes' => true])
-    
-    @foreach($articles as $article)
-      @include('articles.card')
-    @endforeach
+
+  <div class="container mt-4">
+    <div class="row justify-content-center">
+      <div class="col-md-9">
+        @include('users.user')
+        @include('users.tabs', ['hasArticles' => false, 'hasLikes' => true])
+
+        @include('articles.list', compact('articles'))
+
+      </div>
+    </div>
   </div>
+
 @endsection
