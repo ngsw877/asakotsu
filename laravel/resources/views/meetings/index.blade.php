c692d2c475a90e2f45a6097e9ddda6bc93eff203
ngsw877 <otsda1055@yahoo.co.jp>
2020-11-07 13:54:06 +0900

    [clean]ミーティング一覧の無限スクロールスピナーをBlade化

--- laravel/resources/views/meetings/index.blade.php
+++ laravel/resources/views/meetings/index.blade.php
@@ -9,20 +9,11 @@
   <div class="container mt-4">
 
     <div class="row">
-     <div class="mx-auto col-md-8">
-        @include('meetings.list', compact('meetings'))
+      <div class="mx-auto col-md-8">
+          @include('meetings.list', compact('meetings'))
 
-        <!-- 無限スクロールのsppiner -->
-        @if ($meetings->nextPageUrl())
-          <a href="{{ $meetings->nextPageUrl() }}" infinity-scroll>
-            <div class="d-flex justify-content-center my-4">
-              <div class="spinner-border" role="status">
-                <span class="sr-only">Loading...</span>
-              </div>
-            </div>
-          </a>
-        @endif
-     </div>
+          @include('meetings.sppiner')
+      </div>
     </div>
 
     @include('meetings.new_post_btn')
