d1d14bb8eec6e059ec2fcd846d4ff6e893d383e2
ngsw877 <otsda1055@yahoo.co.jp>
2020-11-14 18:44:45 +0900

    [mod]投稿一覧の表示を、未ログイン/ログイン後に分けてテスト

--- laravel/tests/Feature/ArticleControllerTest.php
+++ laravel/tests/Feature/ArticleControllerTest.php
@@ -11,13 +11,35 @@ class ArticleControllerTest extends TestCase
 {
     use RefreshDatabase;
 
-    public function testIndex()
+    ### 投稿一覧表示機能のテスト ###
+
+    // 未ログイン時
+    public function testGuestIndex()
     {
         $response = $this->get(route('articles.index'));
 
         $response->assertStatus(200)
-        ->assertViewIs('articles.index');
+        ->assertViewIs('articles.index')
+        ->assertSee('ユーザー登録')
+        ->assertSee('ログイン')
+        ->assertSee('かんたんログイン');
     }
+
+    // ログイン時
+    public function testAuthIndex()
+    {
+        $user = factory(User::class)->create();
+
+        $response = $this->actingAs($user)
+        ->get(route('articles.index'));
+
+        $response->assertStatus(200)
+        ->assertViewIs('articles.index')
+        ->assertSee('投稿する')
+        ->assertSee($user->name . 'さん')
+        ->assertSee('新規投稿');
+    }
+
 
     public function testGuestCreate()
     {
