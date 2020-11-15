4a6c4829508da46c523fc76fa337839c7b04eab3
ngsw877 <otsda1055@yahoo.co.jp>
2020-11-15 15:57:21 +0900

    [update]タグ毎の投稿一覧で表示する投稿の並び順を、「投稿日が新しい順」に修正

--- laravel/app/Http/Controllers/TagController.php
+++ laravel/app/Http/Controllers/TagController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Tag;
+use App\Models\Article;
 use App\Models\User;
 
 class TagController extends Controller
@@ -11,6 +12,9 @@ class TagController extends Controller
     public function show(string $name, User $user)
     {
         $tag = Tag::where('name', $name)->first();
+
+        // 投稿日が新しい順にソート
+        $tag->articles = $tag->articles->sortByDesc('created_at');
 
         // ユーザーの早起き達成日数ランキングを取得
         $ranked_users = $user->ranking();
