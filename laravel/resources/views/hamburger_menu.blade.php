42df957ebf623929a62f7458219425a1645641ae
ngsw877 <otsda1055@yahoo.co.jp>
2020-11-28 22:45:23 +0900

    [add]ログイン後の、ハンバーガーメニューモーダルを作成

--- laravel/resources/views/hamburger_menu.blade.php
+++ laravel/resources/views/hamburger_menu.blade.php
@@ -12,26 +12,59 @@
   <ul class="nav-modal mb-0 text-center">
   @guest
     <li>
-      <a href="{{ route('register') }}">
+      <a class="waves-effect waves-light" href="{{ route('register') }}">
         <i class="fas fa-user-plus mr-1"></i>
         ユーザー登録
       </a>
     </li>
 
     <li>
-      <a href="{{ route('login') }}">
+      <a class="waves-effect waves-light" href="{{ route('login') }}">
         <i class="fas fa-sign-in-alt mr-1"></i>
         ログイン
       </a>
     </li>
 
     <li class="bg-default rounded">
-      <a class="nav-link waves-effect waves-light" href="{{ route('login.guest') }}">
+      <a class="waves-effect waves-light" href="{{ route('login.guest') }}">
         <i class="fas fa-user-check mr-1"></i>
         かんたんログイン
       </a>
     </li>
   @endguest
+
+  @auth
+      <li>
+        <a class="waves-effect waves-light" href="{{ route('meetings.index') }}">
+          <i class="fas fa-video mr-2"></i>
+          Zoom朝活
+        </a>
+      </li>
+
+      <li>
+        <a class="waves-effect waves-light" href="{{ route('articles.create') }}">
+          <i class="fas fa-pen mr-2"></i>
+          投稿する
+        </a>
+      </li>
+
+      <li>
+        <a class="waves-effect waves-light" onclick="location.href='{{ route("users.show", ["name" => Auth::user()->name]) }}'">
+        <i class="fas fa-user mr-1"></i>
+          マイページ
+        </a>
+      </li>
+      <form id="logout-button" method="POST" action="{{ route('logout') }}">
+        @csrf
+      <li>
+
+        <button form="logout-button" class="waves-effect waves-light text-white" type="submit" style="padding: 15px 10px; width:100%;">
+          <i class="fas fa-sign-out-alt mr-1"></i>
+          ログアウト
+        </button>
+      </li>
+      </form>
+      @endauth
 
   </ul>
 </nav>
