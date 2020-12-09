e62d36c6b8224d29b61d324c79a68fcf413f81c5
ngsw877 <otsda1055@yahoo.co.jp>
2020-12-09 22:36:00 +0900

    [clean]Bladeの共通化

--- laravel/resources/views/meetings/list.blade.php
+++ laravel/resources/views/meetings/list.blade.php
@@ -1,17 +1,7 @@
 @if($meetings->isEmpty())
-<div class="row">
-  <div class="col-md mb-4">
-    <div class="card">
-      <div class="card-body">
-        <p class="text-center text-muted mb-0">ミーティングがありません。</p>
-      </div>
-    </div>
-  </div>
-</div>
+  @include('not_exist', ['message' => 'ミーティングがありません。'])
 @endif
 
 @foreach($meetings as $meeting)
-
-@include('meetings.card')
-
+  @include('meetings.card')
 @endforeach
---
+++ laravel/resources/views/not_exist.blade.php
@@ -0,0 +1,9 @@
+<div class="row">
+  <div class="col-md mb-4">
+    <div class="card">
+      <div class="card-body">
+        <p class="text-center text-muted mb-0">{{ $message }}</p>
+      </div>
+    </div>
+  </div>
+</div>
