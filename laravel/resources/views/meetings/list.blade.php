58e4ed6710fc6128436a88b508b9162c24046a84
ngsw877 <otsda1055@yahoo.co.jp>
2020-12-09 22:14:28 +0900

    [update]ミーティングが0件の場合のBladeを追加

--- laravel/resources/views/meetings/list.blade.php
+++ laravel/resources/views/meetings/list.blade.php
@@ -1,3 +1,15 @@
+@if($meetings->isEmpty())
+<div class="row">
+  <div class="col-md mb-4">
+    <div class="card">
+      <div class="card-body">
+        <p class="text-center text-muted mb-0">ミーティングがありません。</p>
+      </div>
+    </div>
+  </div>
+</div>
+@endif
+
 @foreach($meetings as $meeting)
 
 @include('meetings.card')
