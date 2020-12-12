2a511d7de56d3ac9ca9483c8d656ce2030f6ada9
ngsw877 <otsda1055@yahoo.co.jp>
2020-12-12 16:55:09 +0900

    [clean]コメント機能の不要メソッドを削除し、リファクタリング

--- laravel/app/Models/Comment.php
+++ laravel/app/Models/Comment.php
@@ -17,9 +17,4 @@ class Comment extends Model
     {
         return $this->belongsTo(User::class);
     }
-
-    public function getComments(Int $article_id)
-    {
-        return $this->with('user')->where('article_id', $article_id)->get();
-    }
 }
