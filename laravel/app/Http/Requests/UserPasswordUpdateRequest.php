b7d0293221f09fd166fd842a8acbc3bc45f85ebf
ngsw877 <otsda1055@yahoo.co.jp>
2020-12-26 21:07:09 +0900

    [add]ユーザーパスワード変更機能の、フォームリクエストを作成

---
+++ laravel/app/Http/Requests/UserPasswordUpdateRequest.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class UserPasswordUpdateRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'current_password' => 'required|string|min:8|password:api',
+            'new_password' => 'required|string|min:8|confirmed|different:current_password',
+        ];
+    }
+
+    public function attributes()
+    {
+        return [
+            'current_password' => '現在のパスワード',
+            'new_password' => '新しいパスワード',
+        ];
+    }
+
+}
