051b2f25ddeabd929dbe388f60d19462bd969469
ngsw877 <otsda1055@yahoo.co.jp>
2021-01-03 10:45:32 +0900

    [clean]Blade修正

--- laravel/resources/views/users/edit.blade.php
+++ laravel/resources/views/users/edit.blade.php
@@ -25,7 +25,7 @@
                     <input type="file" name="profile_image" id="profile_image" class="d-none" accept="image/*">
                   </label>
                 </div>
-                @if (Auth::user()->name == 'ゲストユーザー')
+                @if (Auth::user()->email == 'guest@guest.com')
                   <p class="text-danger">※ゲストユーザーは、ユーザー名とメールアドレスを編集できません。</p>
                 @endif
                 <div class="form-group">
@@ -33,7 +33,7 @@
                     ユーザー名
                     <small class="blue-grey-text">（15文字以内）</small>
                   </label>
-                  @if (Auth::user()->name == 'ゲストユーザー')
+                  @if (Auth::user()->email == 'guest@guest.com')
                     <input class="form-control" type="text" id="name" name="name" value="{{ $user->name }}" readonly>
                   @else
                     <input class="form-control" type="text" id="name" name="name" value="{{ $user->name ?? old('name') }}">
@@ -46,20 +46,6 @@
                   @else
                     <input class="form-control" type="text" id="email" name="email" value="{{ $user->email ?? old('email') }}">
                   @endif
-                </div>
-                <div class="form-group">
-                  <label for="wake_up_time">
-                    目標起床時間
-                    <small class="blue-grey-text">（04:00 〜 10:00）</small>
-                    <p class="mb-1 small text-default">※動作確認用に、現在自由に目標起床時間を設定できます。</p>
-                  </label>
-                  <!-- 動作確認用に、目標起床時間の設定可能時間帯の制限を解除中。　min="04:00" max="10:00" -->
-                  <input class="form-control" type="time" id="wake_up_time" name="wake_up_time"
-                  value="{{
-                    null !== old('wake_up_time') ?
-                    Carbon\Carbon::parse(old('wake_up_time'))->format('H:i') :
-                    $user->wake_up_time->format('H:i')
-                  }}">
                 </div>
                 <div class="form-group">
                   <label for="email">
