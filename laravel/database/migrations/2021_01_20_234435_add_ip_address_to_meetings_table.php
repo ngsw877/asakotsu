a72b57559fa6d720a4bbe22ee0ee071a21f042b2
ngsw877 <otsda1055@yahoo.co.jp>
2021-01-22 23:06:45 +0900

    [clean]微修正

--- laravel/database/migrations/2021_01_20_234435_add_ip_address_to_meetings_table.php
+++ laravel/database/migrations/2021_01_20_234435_add_ip_address_to_meetings_table.php
@@ -14,7 +14,7 @@ class AddIpAddressToMeetingsTable extends Migration
     public function up()
     {
         Schema::table('meetings', function (Blueprint $table) {
-            $table->ipAddress('ip_address')->after('user_id')->nullable()->comment('IPアドレス');;
+            $table->ipAddress('ip_address')->after('user_id')->nullable()->comment('IPアドレス');
         });
     }
 
