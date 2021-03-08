28d1c588c71a5ca439c1f42d11bae1b8faa1bf8a
ngsw877 <otsda1055@yahoo.co.jp>
2021-03-08 23:39:19 +0900

    [clean]ミーティングタイプの定数を、configファイルで管理するよう変更

--- laravel/app/Http/Controllers/Zoom/MeetingController.php
+++ laravel/app/Http/Controllers/Zoom/MeetingController.php
@@ -12,11 +12,6 @@ use Carbon\CarbonImmutable;
 class MeetingController extends Controller
 {
 
-    // const MEETING_TYPE_INSTANT = 1;
-    const MEETING_TYPE_SCHEDULE = 2;
-    // const MEETING_TYPE_RECURRING = 3;
-    // const MEETING_TYPE_FIXED_RECURRING_FIXED = 8;
-
     private $client;
     private CarbonImmutable $today;
 
--- laravel/config/zoom.php
+++ laravel/config/zoom.php
@@ -1,12 +1,18 @@
 <?php
 
 return [
-  'zoom_api_url' => env('ZOOM_API_URL'),
-  'zoom_api_key' => env('ZOOM_API_KEY'),
-  'zoom_api_secret' => env('ZOOM_API_SECRET'),
-  'zoom_account_email' => env('ZOOM_ACCOUNT_EMAIL'),
-  'meeting_status' => [
-      'active' => 'started',
-      'inactive' => 'waiting'
-  ]
+    'zoom_api_url' => env('ZOOM_API_URL'),
+    'zoom_api_key' => env('ZOOM_API_KEY'),
+    'zoom_api_secret' => env('ZOOM_API_SECRET'),
+    'zoom_account_email' => env('ZOOM_ACCOUNT_EMAIL'),
+    'meeting_status' => [
+        'active' => 'started',
+        'inactive' => 'waiting'
+    ],
+    'meeting_type' => [
+        'instant_meeting' => 1,
+        'scheduled_meeting' => 2,
+        'recurring_meeting_with_no_fixed_time' => 3,
+        'recurring_meeting_with_fixed_time' => 8
+    ]
 ];
