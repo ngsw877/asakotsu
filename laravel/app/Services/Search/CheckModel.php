95e3f2de446507dcaba01f0bb4797627c67a17a1
ngsw877 <otsda1055@yahoo.co.jp>
2021-03-14 23:16:13 +0900

    [clean]Articleモデル、Meetingモデルからキーワード検索する処理をそれぞれServiceクラスに切り出し

--- laravel/app/Services/Search/CheckModel.php
+++ laravel/app/Services/Search/CheckModel.php
@@ -4,11 +4,25 @@ namespace App\Services\Search;
 
 use App\Models\Article;
 use App\Models\Meeting;
+use App\Services\Search\SearchArticle;
+use App\Services\Search\SearchMeeting;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Model;
 
 class CheckModel
 {
+    private SearchArticle $searchArticle;
+    private SearchMeeting $searchMeeting;
+
+    public function __construct(
+        SearchArticle $searchArticle,
+        SearchMeeting $searchMeeting
+    )
+    {
+        $this->searchArticle = $searchArticle;
+        $this->searchMeeting = $searchMeeting;
+    }
+
     /**
      *  キーワード検索対象のModelを選別し、ページネーションを返す
      * @param array $keyword
@@ -18,24 +32,16 @@ class CheckModel
      */
     public function checkModelForSearchKeyword(array $keywordSplit, Model $model, Builder $query): Builder
     {
-        // ユーザー投稿をキーワードで検索
         if ($model instanceof Article) {
-            // 単語をループで回す
-            foreach($keywordSplit as $value)
-            {
-                $query->where('body','like','%'.$value.'%')
-                    ->with(['user', 'likes', 'tags']);
-            }
+            // ユーザー投稿をキーワードで検索
+            $query = $this->searchArticle
+                ->searchKeywordFromArticle($keywordSplit, $query);
         }
 
-        // ミーティングをキーワードで検索
         if ($model instanceof Meeting) {
-            // 単語をループで回す
-            foreach($keywordSplit as $value)
-            {
-                $query->where('topic','like','%'.$value.'%')
-                    ->orWhere('agenda','like','%'.$value.'%');
-            }
+            // ミーティングをキーワードで検索
+            $query = $this->searchMeeting
+                ->searchKeywordFromMeeting($keywordSplit, $query);
         }
 
         return $query;
---
+++ laravel/app/Services/Search/SearchArticle.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Services\Search;
+
+use App\Models\Article;
+use Illuminate\Database\Eloquent\Builder;
+
+class SearchArticle
+{
+    /**
+     * Articleモデルから、キーワード検索にマッチしたデータを取得
+     * @param array $keywordSplit
+     * @param Builder $query
+     * @return Builder
+     */
+    public function searchKeywordFromArticle(array $keywordSplit, Builder $query): Builder
+    {
+        // ユーザー投稿をキーワードで検索
+        // 単語をループで回す
+        foreach($keywordSplit as $value)
+        {
+            $query->where('body','like','%'.$value.'%')
+                ->with(['user', 'likes', 'tags']);
+        }
+
+        return $query;
+    }
+
+}
