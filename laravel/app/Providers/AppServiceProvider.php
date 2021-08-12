eae46f0b94b7c76cd950cedd329679491590c1a3
name=ngsw877 <otsda1055@yahoo.co.jp>
2021-08-13 01:04:13 +0900

    [add]UserServiceを作成

--- laravel/app/Providers/AppServiceProvider.php
+++ laravel/app/Providers/AppServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace App\Providers;
 
+use App\Services\User\UserService;
+use App\Services\User\UserServiceInterface;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -13,7 +15,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        //
+        $this->app->bind(UserServiceInterface::class,UserService::class);
     }
 
     /**
---
+++ laravel/app/Services/User/UserService.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace App\Services\User;
+
+class UserService implements UserServiceInterface
+{
+
+}
