28ae0db095278919c9154900da9d64efe9aa427a
ngsw877 <otsda1055@yahoo.co.jp>
2021-08-14 14:44:56 +0900

    [remove]不要なルーティングを削除

--- laravel/routes/api.php
+++ laravel/routes/api.php
@@ -3,15 +3,3 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return [
-        'result' => true,
-    ];
-});
-
-// 作成済みミーティング情報を一覧で取得
-Route::get('/meetings/list', 'Zoom\MeetingController@getListMeetings')->name('meetings.list');
-// 作成済みミーティングの、「開始日」と「ステータスをチェック」し、過去のミーティングを削除する
-Route::get('/meetings/check', 'Zoom\MeetingController@checkStartTimeAndStatusOfMeetings')->name('meetings.check');
-// 指定したミーティング情報の取得
-Route::get('/meetings/{meeting_id}', 'Zoom\MeetingController@getMeeting')->name('meetings.get');
