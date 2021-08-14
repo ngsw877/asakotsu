7bb6a6e3ee2e7fea2f48dc5be68d258d66363d72
name=ngsw877 <otsda1055@yahoo.co.jp>
2021-08-14 21:15:06 +0900

    [update]minioに画像アップロードされるよう設定とロジック改修

--- laravel/app/Http/Controllers/Auth/RegisterController.php
+++ laravel/app/Http/Controllers/Auth/RegisterController.php
@@ -96,7 +96,7 @@ class RegisterController extends Controller
 
             $disk = Storage::disk('s3');
             // バケットの`image/profile`フォルダへアップロード
-            $path = $disk->putFile('images/profile', $image, 'public');
+            $path = $disk->putFile('uploads/images/profile', $image, 'public');
             // アップロードした画像のフルパスを取得
             $image_path = $disk->url($path);
         }
