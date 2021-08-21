61623b8c40636e11a9790a2453ba4b0582b07ef9
name=ngsw877 <otsda1055@yahoo.co.jp>
2021-08-21 21:26:57 +0900

    [remove]不要な引数を削除

--- laravel/app/Http/Controllers/CommentController.php
+++ laravel/app/Http/Controllers/CommentController.php
@@ -4,11 +4,10 @@ namespace App\Http\Controllers;
 
 use App\Repositories\User\UserRepositoryInterface;
 use Illuminate\Http\RedirectResponse;
-use App\Models\Comment;
 use App\Http\Requests\CommentRequest;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Log;
-use Mockery\Exception;
+use Exception;
 
 class CommentController extends Controller
 {
@@ -25,10 +24,9 @@ class CommentController extends Controller
      * ユーザーの投稿に、コメントを投稿
      *
      * @param CommentRequest $request
-     * @param Comment $comment
      * @return RedirectResponse
      */
-    public function store(CommentRequest $request, Comment $comment): RedirectResponse
+    public function store(CommentRequest $request): RedirectResponse
     {
         // 二重送信対策
         $request->session()->regenerateToken();
