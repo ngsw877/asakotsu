d07a01fca15714f8ab5e978f75ee1501579bbdf8
name=ngsw877 <otsda1055@yahoo.co.jp>
2021-08-22 23:03:38 +0900

    [add]メインタグを取得するメソッドを作成

--- laravel/app/Repositories/Tag/TagRepository.php
+++ laravel/app/Repositories/Tag/TagRepository.php
@@ -2,7 +2,25 @@
 
 namespace App\Repositories\Tag;
 
+use App\Models\Tag;
+use Illuminate\Support\Collection;
+
 class TagRepository implements TagRepositoryInterface
 {
+    private Tag $tag;
 
+    public function __construct(Tag $tag)
+    {
+        $this->tag = $tag;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function getMainTags(): Collection
+    {
+        return $this->tag
+            ->whereIn('name', config('tag.main'))
+            ->get();
+    }
 }
--- laravel/app/Repositories/Tag/TagRepositoryInterface.php
+++ laravel/app/Repositories/Tag/TagRepositoryInterface.php
@@ -2,7 +2,15 @@
 
 namespace App\Repositories\Tag;
 
+use Illuminate\Support\Collection;
+
 interface TagRepositoryInterface
 {
 
+    /**
+     * メインタグをCollectionで取得
+     *
+     * @return Collection
+     */
+    public function getMainTags(): Collection;
 }
