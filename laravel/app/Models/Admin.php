45af9b204ef02cdb9e152596f897d69ad66d808e
name=ngsw877 <otsda1055@yahoo.co.jp>
2021-08-25 22:57:49 +0900

    [add]管理者アカウント用に、adminsテーブルを作成

---
+++ laravel/app/Models/Admin.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Admin extends Model
+{
+    protected $fillable = [
+        'name',
+        'email',
+        'password'
+    ];
+}
