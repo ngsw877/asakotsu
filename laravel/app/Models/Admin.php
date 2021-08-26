13a74fed67718a53836f926ce765498616fea5d9
name=ngsw877 <otsda1055@yahoo.co.jp>
2021-08-26 22:31:17 +0900

    [add]use SoftDeletesを追加

--- laravel/app/Models/Admin.php
+++ laravel/app/Models/Admin.php
@@ -3,9 +3,12 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Admin extends Model
 {
+    use SoftDeletes;
+
     protected $fillable = [
         'name',
         'email',
