05a816d29a1883178f39da8a0adb7aeb7cebfc8e
name=ngsw877 <otsda1055@yahoo.co.jp>
2021-08-27 22:53:18 +0900

    [add]管理者ユーザーのSeederを作成

---
+++ laravel/database/seeds/AdminsTableSeeder.php
@@ -0,0 +1,22 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class AdminsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('admins')->insert([
+            'name'              => 'admin',
+            'email'             => 'admin@example.com',
+            'password'          => Hash::make('12345678'),
+            'remember_token'    => Str::random(10),
+        ]);
+    }
+}
