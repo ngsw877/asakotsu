0d720ab7d8ef76c8c35edb9718ef24ae3c903f37
name=ngsw877 <otsda1055@yahoo.co.jp>
2021-08-28 00:23:55 +0900

    [add]admin用のroutingファイルを作成

---
+++ laravel/app/Http/Controllers/Admin/HomeController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class HomeController extends Controller
+{
+    public function __construct()
+    {
+        $this->middleware('auth:admin');
+    }
+
+    public function index()
+    {
+        return view('admin.home');
+    }
+}
---
+++ laravel/routes/admin.php
@@ -0,0 +1,20 @@
+<?php
+
+Route::namespace('Admin')->prefix('admin')->name('admin.')->group(function () {
+
+    // ログイン認証関連
+    Auth::routes([
+        'register' => true,
+        'reset'    => false,
+        'verify'   => false
+    ]);
+
+    // ログイン認証後
+    Route::middleware('auth:admin')->group(function () {
+
+        // TOPページ
+        Route::get('home', 'HomeController@index')->name('name');
+
+    });
+
+});
