c458f343a4d10058a45273c7687a7c3dffe2c739
name=ngsw877 <otsda1055@yahoo.co.jp>
2021-08-29 00:47:07 +0900

    [update]管理画面に表示する、認証関係のURLを調整

--- laravel/config/adminlte.php
+++ laravel/config/adminlte.php
@@ -188,12 +188,12 @@ return [
     */
 
     'use_route_url' => false,
-    'dashboard_url' => 'home',
-    'logout_url' => 'logout',
-    'login_url' => 'login',
-    'register_url' => 'register',
-    'password_reset_url' => 'password/reset',
-    'password_email_url' => 'password/email',
+    'dashboard_url' => 'admin/home',
+    'logout_url' => 'admin/logout',
+    'login_url' => 'admin/login',
+    'register_url' => 'admin/register',
+    'password_reset_url' => 'admin/password/reset',
+    'password_email_url' => 'admin/password/email',
     'profile_url' => false,
 
     /*
