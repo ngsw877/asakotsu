92ad28d67b945f25556dbce7f01b01b5de2b7373
name=ngsw877 <otsda1055@yahoo.co.jp>
2021-08-28 23:54:22 +0900

    [add]マルチ認証用にMiddlewareを実装

--- laravel/app/Http/Middleware/Authenticate.php
+++ laravel/app/Http/Middleware/Authenticate.php
@@ -3,19 +3,29 @@
 namespace App\Http\Middleware;
 
 use Illuminate\Auth\Middleware\Authenticate as Middleware;
+use Illuminate\Support\Facades\Route;
 
 class Authenticate extends Middleware
 {
+    protected string $userRoute = 'login';
+    protected string $adminRoute = 'admin.login';
+
     /**
      * Get the path the user should be redirected to when they are not authenticated.
+     * 未ログインユーザーが、ログイン状態でないとアクセスできないURLに移動しようとした時のリダイレクト先を指定
      *
-     * @param  \Illuminate\Http\Request  $request
+     * @param \Illuminate\Http\Request $request
      * @return string|null
      */
     protected function redirectTo($request)
     {
+        // ルーティングに応じて未ログイン時のリダイレクト先を振り分ける
         if (! $request->expectsJson()) {
-            return route('login');
+            if (Route::is('user.*')) {
+                return route($this->userRoute);
+            } elseif (Route::is('admin.*')) {
+                return route($this->adminRoute);
+            }
         }
     }
 }
