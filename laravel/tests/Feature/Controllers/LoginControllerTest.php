04f95e47cb2cba1849dcd5dbfbb5b52774e105a2
name=ngsw877 <otsda1055@yahoo.co.jp>
2021-09-23 00:16:26 +0900

    [add]ログイン時に、バリデーションエラーが適切に発生するかのテストを追加

--- laravel/tests/Feature/Controllers/LoginControllerTest.php
+++ laravel/tests/Feature/Controllers/LoginControllerTest.php
@@ -2,12 +2,15 @@
 
 namespace Tests\Feature\Controllers;
 
+use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 
 class LoginControllerTest extends TestCase
 {
+    use RefreshDatabase;
+
     /**
      * ログイン画面を開ける
      *
@@ -18,5 +21,53 @@ class LoginControllerTest extends TestCase
         $response = $this->get(route('login'));
 
         $response->assertStatus(200);
+    }
+
+    /**
+     * ログイン時にバリデーションエラーが適切に発生する
+     */
+    public function testValidation()
+    {
+        $loginUrl = 'login';
+
+        $this->from($loginUrl)
+            ->post($loginUrl, [])
+            ->assertRedirect($loginUrl);
+
+        $this->post($loginUrl, ['email' => ''])->assertSessionHasErrors(['email' => 'メールアドレスは必ず指定してください。']);
+        $this->post($loginUrl, ['password' => ''])->assertSessionHasErrors(['password' => 'パスワードは必ず指定してください。']);
+
+
+        // DBに登録されていないメールアドレス・パスワードを入力してログインしようとすると、バリデーションエラーが発生することをテスト
+        $dbData = [
+            'email'    => 'aaa@example.com',
+            'password' => 'Test1234',
+        ];
+
+        // DBにユーザーを登録
+        factory(User::class)->create($dbData);
+
+        $postDataSet = [
+            [
+                'email'    => 'bbb@example.com', // メールアドレスが間違っている
+                'password' => 'Test1234',
+            ],
+            [
+                'email'    => 'aaa@example.com',
+                'password' => 'hoge5678', // パスワードが間違っている
+            ],
+            [
+                // メールアドレスもパスワードも両方間違っている
+                'email'    => 'bbb@example.com',
+                'password' => 'hoge5678',
+            ],
+        ];
+
+        foreach ($postDataSet as $postData) {
+            $this->from($loginUrl)
+                ->followingRedirects()
+                ->post($loginUrl, $postData)
+                ->assertSee('ログイン情報が登録されていません。');
+        }
     }
 }
